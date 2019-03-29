ff252662d00391cb242fcf9d8d84b3cba1f11e5d
unknown <VladMoisescu96>
2019-03-29 10:15:44 +0200

    first commit

---
+++ PracticalTest01Var05/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/MainActivity.java
@@ -0,0 +1,15 @@
+package ro.pub.cs.systems.eim.practicaltest01var05;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
